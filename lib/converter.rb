dff4adc6e0d33f606aa781c2d7baf839ec0db2a8
Eric VanWieren <evanwieren@gmail.com>
2013-08-31 10:10:22 -0500

    This xml pwx file is valid. Beginning to write a file. Still not working. and very ugly.

--- lib/converter.rb
+++ lib/converter.rb
@@ -1,10 +1,18 @@
 #!/usr/bin/env ruby
 
 require "rexml/document"
+require "builder"
 #file = File.new( "mydoc.xml" )
 #doc = REXML::Document.new file
 
 doc = REXML::Document.new File.new(ARGV[0])
+
+xml = Builder::XmlMarkup.new( :target => $stdout, :indent => 2 )
+xml.instruct! :xml, :version => "1.1", :encoding => "US-ASCII"
+
+count = 0
+
+xml.workout do
 doc.elements.each('pwx/workout/sample') do |foo|
 	#puts foo.element('timeoffset')
 	#puts foo.methods
@@ -12,12 +20,29 @@ doc.elements.each('pwx/workout/sample') do |foo|
 	sample = {}
 
 	#puts foo.attributes
+		xml.sample(count)
 
 	foo.each do |item| 
 		sample[item.name] = item.text
 	end
 
-	puts sample
+	#puts sample
 
+	count += 1
 
 end
+end
+
+#favorites = {
+#  'candy' => 'Neccos', 'novel' => 'Empire of the Sun', 'holiday' => 'Easter'
+#}
+#
+#
+#
+#xml.favorites do 
+# favorites.each do | name, choice |
+#  xml.favorite( choice, :item => name )
+# end
+#end
+
+
