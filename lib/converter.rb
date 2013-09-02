3435589324121e69690a05e249cb94be6dab6763
Eric VanWieren <evanwieren@gmail.com>
2013-09-02 08:46:48 -0500

    Figured out how to add nested elements. Must use builder

--- lib/converter.rb
+++ lib/converter.rb
@@ -10,39 +10,44 @@ doc = REXML::Document.new File.new(ARGV[0])
 xml = Builder::XmlMarkup.new( :target => $stdout, :indent => 2 )
 xml.instruct! :xml, :version => "1.0", encoding: "UTF-8", standalone: "no"
 
+
 count = 0
 
-xml.workout do
-doc.elements.each('pwx/workout/sample') do |foo|
-	#puts foo.element('timeoffset')
-	#puts foo.methods
-	#puts foo.class
-	sample = {}
+xml.TrainingCenterDatabase do
+	xml.Activities do
+		doc.elements.each('pwx/workout/sample') do |foo|
+			#puts foo.element('timeoffset')
+			sample = {}
 
-	#puts foo.attributes
-		xml.sample(count)
+			#puts foo.attributes
+				xml.sample(count)
 
-	foo.each do |item| 
-		sample[item.name] = item.text
+			foo.each do |item| 
+				sample[item.name] = item.text
+			end
+
+			#puts sample
+
+			count += 1
+
+		end
 	end
-
-	#puts sample
-
-	count += 1
-
 end
-end
+
+#!/usr/bin/ruby
+
+#require 'builder'
 
 #favorites = {
 #  'candy' => 'Neccos', 'novel' => 'Empire of the Sun', 'holiday' => 'Easter'
 #}
-#
-#
-#
+
+#xml = Builder::XmlMarkup.new( :target => $stdout, :indent => 2 )
+
+#xml.instruct! :xml, :version => "1.1", :encoding => "US-ASCII"
+
 #xml.favorites do 
 # favorites.each do | name, choice |
 #  xml.favorite( choice, :item => name )
 # end
 #end
-
-
